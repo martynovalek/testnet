b50460dfa8e8df8e543e85db4b9ef8092f8fb742
Aleks <88688304+martynovalek@users.noreply.github.com>
2022-08-03 20:13:25 +0300

    Update minima_restart.sh

--- minima_restart.sh
+++ minima_restart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo systemctl restart minima_9001
+# curl -s $(wget -qO- eth0.me):9002/incentivecash | jq -r .response.uid > $HOME/minima_uid.txt
 sleep 180
-curl -s $(wget -qO- eth0.me):9002/incentivecash | jq -r .response.uid > $HOME/minima_uid.txt
-sleep 3
 kill -TERM $(pgrep cpulimit)
 sleep 2
-sudo cpulimit -b -p $(pgrep java) -l 10 ; cd
+sudo cpulimit -b -p $(pgrep java) -l 10 && \ 
+cd $HOME
 sleep 5
 curl 127.0.0.1:9002/incentivecash+uid:$(cat $HOME/minima_uid.txt)
